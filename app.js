f123431bf6ee2f8471da3790985cef02a8d6c03f
pkro <pkrombach@web.de>
2021-01-03 10:13:09 +0100

    added interactivity

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 var ol = 0;
 var circleInterval;
+var isPlaying = false;
+var playingBG = document.getElementById('circle').style.backgroundColor;
+var stoppedBG = 'dimgrey';
+
+document.getElementById('circle').style.backgroundColor = stoppedBG;
 
 function circleAnimation(time) {
   ol++;
   document.getElementById('circle').style.left = ol + 'px';
-  if (time < 4000) {
+  circleInterval = requestAnimationFrame(circleAnimation);
+}
+
+function mouseClicked() {
+  isPlaying = !isPlaying;
+  console.log(this.type);
+  if (isPlaying) {
+    document.getElementById('circle').style.backgroundColor = playingBG;
     circleInterval = requestAnimationFrame(circleAnimation);
   } else {
-    document.getElementById('circle').style.backgroundColor = 'dimgrey';
-    // works fine without this
+    document.getElementById('circle').style.backgroundColor = stoppedBG;
     cancelAnimationFrame(circleInterval);
   }
 }
-circleInterval = requestAnimationFrame(circleAnimation);
+
+document.getElementById('circle').addEventListener('click', mouseClicked);

1ad363f6b2a8cb1a2407de9e814d0ad365516b07
pkro <pkrombach@web.de>
2021-01-05 09:24:16 +0100

    done

--- app.js
+++ app.js
@@ -1,44 +1,31 @@
-(function () {
-  /** @type {HTMLCanvasElement} */
-  var canvas = document.getElementById('animated-canvas');
-  var context = canvas.getContext('2d');
-  const { width: w, height: h } = context.canvas;
-  const { PI: π } = Math;
-  var anim = null;
+var rotatingSquare = (function () {
+  // only export what's necessary
+  'use strict';
 
-  let numBalls = 3 + Math.floor(Math.random() * 3);
-  let balls = Array(numBalls).fill({
-    velocity: 50,
-    x: 60,
-    acceleration: null,
-  });
-
-  balls = balls.map((ball) => ({
-    ...ball,
-    acceleration: Math.random(),
-  }));
-
-  function drawCircle(x, y, radius) {
-    context.fillStyle = 'crimson';
-    context.beginPath();
-    context.arc(x, y, radius, 0, 2 * π);
-    context.fill();
-  }
-
-  function animate(time) {
-    //context.clearRect(0, 0, w, h);
-    //context.globalCompositeOperation = 'difference';
-    context.globalCompositeOperation = 'screen';
-    balls.forEach((ball, idx) => {
-      if (ball.x > w) {
-        ball.x = -50;
-      }
-      ball.velocity += ball.acceleration;
-      ball.x += ball.velocity;
-      drawCircle(ball.x, (idx + 1) * 50 + 50, 25);
-    });
-
-    anim = requestAnimationFrame(animate);
-  }
-  anim = requestAnimationFrame(animate);
+  var rotatingSquare = document.getElementById('web-animation').animate(
+    [
+      {
+        backgroundColor: '#000000',
+        opacity: 1.0,
+        transform: 'rotate(0deg) translate3D(0,0,0)',
+        //easing: 'ease-out',
+      },
+      {
+        backgroundColor: '#440000',
+        opacity: 0.3,
+      },
+      {
+        backgroundColor: '#000000',
+        opacity: 1.0,
+        transform: 'rotate(360deg) translate3D(0,0,0)',
+        //easing: 'ease-in',
+      },
+    ],
+    {
+      duration: 500,
+      iterations: 1000,
+      easing: 'ease-in-out',
+    }
+  );
+  return rotatingSquare;
 })();
--- readme.md
+++ readme.md
@@ -12,6 +12,31 @@
 - seems optional, answer: *"To sum up, you SHOULD use cancelAnimationFrame to prevent side effects from a function auto-scheduled with requestAnimationFrame, and you CAN optionally use it to (micro-)optimize by avoiding its execution even if it has no side effects."* 
   *https://stackoverflow.com/questions/27108333/why-would-i-ever-need-to-cancelanimationframe*
 
+### web animations api
 
+- basically the same as doing it by css / keyframes; keyframes can be defined in array of keyframe objects
+
+  var rotatingSquare = document.getElementById('web-animation');
+
+      rotatingSquare.animate([
+        {
+          backgroundColor: '#000000',
+        },
+        {
+          backgroundColor: '#440000',
+        },
+        {
+          backgroundColor: '#000000',
+        },
+      ],
+      {
+        duration: 1000,
+        iterations: 7
+      });
+
+- without timings the defined keyframes are spaced equally (with 3 keyframes for example at 0%, 50%, 100%)
+- The Element interface's animate() method is a shortcut method which creates a new Animation, applies it to the element, then plays the animation. It returns the created Animation object instance (so be sure to assign it to a variable, as the element returned by getElementById (or similar) will not have the animation methods if animate wasn't called on it first)
+  
 # Sidenotes
+
 - element.style.width seems to return an empty string when reading it, workaround was element.offsetWidth
